32495b6bf73855e8585fa9288d9a024c8af8d613
Supriya Kashyap <kashyapsupriya61275@gmail.com>
2023-08-01 23:02:54 +0530

    navBar

--- app/src/main/java/com/example/xaper_x/MainActivity.kt
+++ app/src/main/java/com/example/xaper_x/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.xaper_x
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.xaper_x.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        window.navigationBarColor = getColor(android.R.color.white)
+        binding.bottomNav.setOnItemSelectedListener {
+            Toast.makeText(this@MainActivity,"Item Clicked",Toast.LENGTH_SHORT).show()
+            return@setOnItemSelectedListener true
+        }
+
     }
 }
