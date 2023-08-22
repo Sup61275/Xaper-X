d5bc12cb646ffbc62cb1e075c9f7f43c98c9e86a
Supriya Kashyap <kashyapsupriya61275@gmail.com>
2023-08-23 00:08:24 +0530

    UI

---
+++ app/src/main/java/com/example/xaper_x/PlayerActivity.kt
@@ -0,0 +1,54 @@
+package com.example.xaper_x
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.xaper_x.databinding.ActivityPlayerBinding
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
+
+class PlayerActivity : AppCompatActivity() {
+    lateinit var binding:ActivityPlayerBinding
+    companion object{
+       lateinit var player:SimpleExoPlayer
+       lateinit var playerList:ArrayList<Video>
+       var position: Int=-1
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityPlayerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initializeLayout()
+
+    }
+
+    private fun initializeLayout() {
+        when(intent.getStringExtra("class")){
+            "AllVideos"->{
+                playerList= ArrayList()
+                playerList.addAll(MainActivity.videoList)
+            }
+            "FolderActivity"-> {
+                playerList = ArrayList()
+                playerList.addAll(FoldersActivity.currentFolderVideos)
+            }
+        }
+        createPlayer()
+    }
+
+    private fun createPlayer(){
+        player = SimpleExoPlayer.Builder(this).build()
+        binding.playerView.player = player
+        val mediaItem = MediaItem.fromUri(playerList[position].artUri)
+        player.setMediaItem(mediaItem)
+        player.prepare()
+        player.play()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        player.release()
+    }
+
+    }
--- app/src/main/java/com/example/xaper_x/VideoAdapter.kt
+++ app/src/main/java/com/example/xaper_x/VideoAdapter.kt
@@ -1,20 +1,23 @@
 package com.example.xaper_x
 
 import android.content.Context
+import android.content.Intent
 import android.text.format.DateUtils
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.example.xaper_x.databinding.VideoViewBinding
 
-class VideoAdapter(private val context: Context, private var videoList: ArrayList<Video>): RecyclerView.Adapter<VideoAdapter.MyHolder>() {
+class VideoAdapter(private val context: Context, private var videoList: ArrayList<Video>,private val isFolder:Boolean=false): RecyclerView.Adapter<VideoAdapter.MyHolder>() {
     class MyHolder (binding: VideoViewBinding) : RecyclerView.ViewHolder(binding.root) {
  val title=binding.videoName
         val folder= binding.folderName
         val duration= binding.duration
         val image=binding.videoImg
+        val root=binding.root
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyHolder {
@@ -30,11 +33,26 @@ class VideoAdapter(private val context: Context, private var videoList: ArrayLis
             .load(videoList[position].artUri)
             .apply(RequestOptions().placeholder(R.mipmap.ic_launcher_round).centerCrop())
             .into(holder.image)
+         holder.root.setOnClickListener {
+when {
+    isFolder->{
+        sendIntent(pos=position,ref="FolderActivity")
+    }else->{
+        sendIntent(pos= position,ref="AllVideos")
+    }
+}
 
+         }
     }
 
     override fun getItemCount(): Int {
      return videoList.size
+    }
+    private fun sendIntent(pos: Int,ref: String){
+        PlayerActivity.position=pos
+        val intent=Intent(context,PlayerActivity::class.java)
+        intent.putExtra("class",ref)
+        ContextCompat.startActivity(context,intent,null)
     }
 
 
